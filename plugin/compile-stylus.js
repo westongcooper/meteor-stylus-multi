3afe21c2375055e0dd63bb5ca7b57b8d1fe477df
Hunter Trujillo <faustianightmare@gmail.com>
2014-08-24 16:23:10 -0600

    Fixes Meteor 0.9 compatibility.

--- plugin/compile-stylus.js
+++ plugin/compile-stylus.js
@@ -9,15 +9,6 @@ var platonic = Npm.require('stylus-platonic');
 var path = Npm.require('path');
 
 Plugin.registerSourceHandler("styl", function(compileStep) {
-  // XXX annoying that this is replicated in .css, .less, and .styl
-  if (!compileStep.archMatches('browser')) {
-    // XXX in the future, might be better to emit some kind of a
-    // warning if a stylesheet is included on the server, rather than
-    // silently ignoring it. but that would mean you can't stick .css
-    // at the top level of your app, which is kind of silly.
-    return;
-  }
-
   var f = new Future;
   stylus(compileStep.read().toString('utf8'))
     .use(nib())
