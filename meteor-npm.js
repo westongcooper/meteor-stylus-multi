ec70b4f993c74bdf8fa60b66a6d0b8b8d8e09fd6
Hunter Trujillo <cryptoquick@gmail.com>
2014-10-18 15:52:42 -0600

    Add a meteor-to-npm package.json converter for shields.

---
+++ meteor-npm.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+
+Package = {};
+
+var content = [];
+
+Package.describe = Package._transitional_registerBuildPlugin = Package.onTest = function (contents) {
+  content.push(contents);
+};
+
+require('./package.js');
+
+var desc = content[0];
+var pkg = content[1];
+
+var json = {
+  name: desc.name,
+  version: desc.version,
+  description: desc.summary,
+  repository: {
+    type: 'git',
+    url: desc.git
+  },
+  dependencies: pkg.npmDependencies,
+  private: true
+};
+
+var str = JSON.stringify(json, null, 2);
+
+fs.writeFile('package.json', str, function () {
+  console.log(str);
+});
